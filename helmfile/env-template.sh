44be96a4d0787e789e63b929e9e253efbead5a80
Anett Hübner <anett_huebner@gmx.de>
2024-12-12 16:41:00 +0100

    added internal registry

--- helmfile/env-template.sh
+++ helmfile/env-template.sh
@@ -2,3 +2,7 @@
 export CLIENT_SECRET=aicockpit
 export KEYCLOAK_ADMIN=aicockpit
 export KEYCLOAK_ADMIN_PW=aicockpit
+export MINIO_PW=minioadmin
+
+export PULL_SECRET=secret # Starwit's internal Docker registry
+export HELM_SECRET=secret # Starwit's internal Helm registry
