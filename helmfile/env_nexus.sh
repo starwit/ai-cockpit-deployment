fe985c4a5af3b3edee3e4346933aca7a69842714
ztarbug <kronprinzfriedrich@gmx.de>
2025-02-05 13:07:21 +0100

    updated component versions, updated github uris

---
+++ helmfile/env_nexus.sh
@@ -0,0 +1,7 @@
+export CLIENT_SECRET=amiANbBrev8mg7TqyG
+export KEYCLOAK_ADMIN_PW=ju633Zs0DQl5TUVrBG
+export KEYCLOAK_REALM_PW=u8cFj4EEmGKI3jrhlC
+export MINIO_PW=iXidSpyIuCtvKZ4UzY
+
+export PULL_SECRET=v3nA86H2YCv6ujBXYWcR # Starwit's internal Docker registry
+export HELM_SECRET=6Sp2gaGadFkfwMy7Tz3e # Starwit's internal Helm registry
