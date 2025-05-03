3eba85df88f8a779ae26e694c6e71b9d6a9b29d7
ztarbug <kronprinzfriedrich@gmx.de>
2025-05-03 11:26:06 +0200

    adapted import script to add data for multiple buckets

--- docker-compose/sample_data_image/import_binary_data.sh
+++ docker-compose/sample_data_image/import_binary_data.sh
@@ -1,11 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Importing binary data from $4 to minio at $1"
+main() {
+    echo "check if there is subfolders"
 
-/bin/mc alias set myminio $1 $2 $3
-/bin/mc mb -p myminio/anomalies
-/bin/mc ls myminio
-/bin/mc policy set public myminio/anomalies
-/bin/ls -al $4
-/bin/mc cp --recursive $4/* myminio/anomalies
-exit 0
+    local folder_path="$1"
+    if [[ -d "$folder_path" ]]; then
+        local subfolders
+        subfolders=$(find "$folder_path" -mindepth 1 -maxdepth 1 -type d)
+        if [[ -n "$subfolders" ]]; then
+            echo "Total subfolders: $(echo "$subfolders" | wc -l)"
+            echo "create minio bucket each"
+            create_buckets "$subfolders" $2 $3 $4
+        else
+            echo "No subfolders found."
+            echo "Total subfolders: 0"
+            setup_single_bucket "$@"
+        fi
+    else
+        echo "Error: '$folder_path' is not a valid directory."
+    fi
+}
+
+create_buckets() {
+    path_array=($1)
+    mc alias set myminio $2 $3 $4
+
+    for i in "${path_array[@]}"
+    do
+        bucket_name=`basename $i`
+        echo "creating bucket for folder $bucket_name"
+        mc mb myminio/$bucket_name
+        mc policy set public myminio/$bucket_name
+        mc cp --recursive $i/* myminio/$bucket_name
+    done 
+}
+
+setup_single_bucket() {
+    echo "Importing binary data from $4 to minio at $1"
+
+    /bin/mc alias set myminio $1 $2 $3
+    /bin/mc mb -p myminio/anomalies
+    /bin/mc ls myminio
+    /bin/mc policy set public myminio/anomalies
+    /bin/ls -al $4
+    /bin/mc cp --recursive $4/* myminio/anomalies
+    exit 0
+}
+
+main "$@"
